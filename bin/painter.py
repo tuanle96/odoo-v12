ab98f8d751f8ab19498ca5e21dc5e3a6239e55dd
Gersonaltamirano <gersonaltamirano@gmail.com>
2019-06-26 17:09:15 +0000

    first commit

---
+++ bin/painter.py
@@ -0,0 +1,4 @@
+#!/opt/bitnami/python/bin/python3
+# EASY-INSTALL-SCRIPT: 'Pillow==4.0.0','painter.py'
+__requires__ = 'Pillow==4.0.0'
+__import__('pkg_resources').run_script('Pillow==4.0.0', 'painter.py')
